bb6ac0df9cba7e6b8d7fd3ffeebb22c2c2585efd
jogoshugh <jgough@JGough.corp.versionone.net>
2013-06-15 17:29:43 -0400

    email

--- mentorSignUp/src/server/app/serverClass.js
+++ mentorSignUp/src/server/app/serverClass.js
@@ -40,8 +40,8 @@
         text: text,
         html: htmlToV1
       });
-      headerText = "Thank you " + req.body.firstName + " " + req.body.lastName + " for volunteering with CoderDojo Ponce Springs! Please fill out the attached background check authorization form and return it to us by following the instructions within it.\n\nHere is a copy of the information you submitted to us:\n\n";
-      headerHtml = "Thank you " + req.body.firstName + " " + req.body.lastName + " for volunteering with CoderDojo Ponce Springs! <b>Please fill out the attached background check authorization form and return it to us by following the instructions within it.</b><br/><br/><hr/><br/>Here is a copy of the information you submitted to us:<br/><br/>";
+      headerText = "" + req.body.firstName + " " + req.body.lastName + ", thanks for volunteering with CoderDojo Ponce Springs! Please fill out the attached background check authorization form and return it to us by following the instructions within it.\n\nHere is a copy of the information you submitted to us:\n\n";
+      headerHtml = "" + req.body.firstName + " " + req.body.lastName + ", thanks for volunteering with CoderDojo Ponce Springs! <b>Please fill out the attached background check authorization form and return it to us by following the instructions within it.</b><br/><br/><hr/><br/>Here is a copy of the information you submitted to us:<br/><br/>";
       text = headerText + signup;
       html = headerHtml + html;
       mailForVolunteer = new sg.Email({
